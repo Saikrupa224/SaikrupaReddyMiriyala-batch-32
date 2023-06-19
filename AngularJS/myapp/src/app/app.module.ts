b5b7c68685d58374993fb7dac809cad29ff3e803
saikrupa224 <saikrupareddymiriyala@gmail.com>
2023-06-19 11:39:01 -0500

    Added

--- AngularJS/myapp/src/app/app.module.ts
+++ AngularJS/myapp/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { LoginComponent } from './Login/Login.component';
+
 
 
 @NgModule({
@@ -12,6 +14,7 @@ import { NavigationComponent } from './navigation/navigation.component';
     AppComponent,
       HeaderComponent,
       NavigationComponent,
+      LoginComponent,
       
    ],
   imports: [
